40c11867ebf3878cea5cee509d38b2cb53b77471
wanghaiyang <39408976+wanghaiyang01@users.noreply.github.com>
2018-09-16 21:16:52 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
    */
     
    //define your token
-   define("TOKEN", "weixinfirst");
+   define("TOKEN", "weixin_first");
    $wechatObj = new wechatCallbackapiTest();
    //$wechatObj->valid();//接口验证
  $wechatObj->responseMsg();//调用回复消息方法
